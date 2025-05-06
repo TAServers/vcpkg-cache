fa6c6de73ad836d7b887987785e989658d9974eb
Derpius <49565664+Derpius@users.noreply.github.com>
2025-05-06 15:16:43 +0100

    Mock path resolve

--- tests/helpers.test.js
+++ tests/helpers.test.js
@@ -1,13 +1,23 @@
-import { expect, test } from "vitest";
+import { expect, test, vi } from "vitest";
 import { getCacheKey, getCacheRestorePath } from "../src/helpers";
+import { resolve } from "path";
+
+vi.mock("path", async (importOriginal) => {
+  const original = await importOriginal();
+
+  return {
+    ...original,
+    resolve: vi.fn().mockReturnValue("/foo/bar"),
+  };
+});
 
 test("getCacheRestorePath should return binary archive path", () => {
-  const vcpkgArchivePath = "/foo/bar";
   const cacheKey = "vcpkg/0cf4d6a517d4d8a3014b4f7e3ff721677c12f9bf443ce894521db388d8f2506b";
 
-  const path = getCacheRestorePath(vcpkgArchivePath, cacheKey);
+  const path = getCacheRestorePath(cacheKey);
 
-  expect(path).toBe(`${vcpkgArchivePath}/0c/0cf4d6a517d4d8a3014b4f7e3ff721677c12f9bf443ce894521db388d8f2506b.zip`);
+  expect(path).toBe("/foo/bar/0c/0cf4d6a517d4d8a3014b4f7e3ff721677c12f9bf443ce894521db388d8f2506b.zip");
+  expect(resolve).toHaveBeenCalledWith(".vcpkg-cache");
 });
 
 test("getCacheKey should return key for filename", () => {
